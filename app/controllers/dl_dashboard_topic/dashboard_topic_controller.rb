b6a5bb76ca52392b943d836a3ba7b1cce7ff4675
Joe Buhlig <joe@joebuhlig.com>
2017-02-17 07:06:18 -0600

    Add headers to response

--- app/controllers/dl_dashboard_topic/dashboard_topic_controller.rb
+++ app/controllers/dl_dashboard_topic/dashboard_topic_controller.rb
@@ -3,30 +3,13 @@ module DlDashboardTopic
     requires_plugin 'dl-dashboard-topic'
     
     skip_before_filter :preload_json, :check_xhr, :redirect_to_login_if_required, :verify_authenticity_token
-    before_filter :cors_preflight_check
-    after_filter :cors_set_access_control_headers
 
     def index
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
+      response.headers['Access-Control-Allow-Headers'] = '*'
       text = Topic.find(SiteSetting.dashboard_topic_id).first_post.cooked
       render json: {cooked: text}
-    end
-
-    def cors_set_access_control_headers
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-      headers['Access-Control-Max-Age'] = "1728000"
-    end
-
-    def cors_preflight_check
-      if request.method == 'OPTIONS'
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
-        headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-        headers['Access-Control-Max-Age'] = '1728000'
-
-        render :text => '', :content_type => 'text/plain'
-      end
     end
 
   end
