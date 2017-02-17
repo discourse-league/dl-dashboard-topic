5cec3b0d6a431c4902f0d5dc81a974e909839a60
Joe Buhlig <joe@joebuhlig.com>
2017-02-16 20:47:25 -0600

    Add request headers

--- app/controllers/dl_dashboard_topic/dashboard_topic_controller.rb
+++ app/controllers/dl_dashboard_topic/dashboard_topic_controller.rb
@@ -5,6 +5,9 @@ module DlDashboardTopic
     skip_before_filter :check_xhr, only: [:index]
 
     def index
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
       text = Topic.find(SiteSetting.dashboard_topic_id).first_post.cooked
       render json: {cooked: text}
     end
