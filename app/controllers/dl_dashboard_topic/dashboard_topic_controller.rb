8b676e64036cd32bae216bae5921a5559f8dfcc4
Joe Buhlig <hello@joebuhlig.com>
2017-10-17 11:35:47 -0500

    Change out headers

--- app/controllers/dl_dashboard_topic/dashboard_topic_controller.rb
+++ app/controllers/dl_dashboard_topic/dashboard_topic_controller.rb
@@ -3,13 +3,16 @@ module DlDashboardTopic
     requires_plugin 'dl-dashboard-topic'
     
     skip_before_action :preload_json, :check_xhr, :redirect_to_login_if_required, :verify_authenticity_token
+    before_action :add_allow_headers, only: [:index]
+
+    def add_allow_headers
+      response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'   
+      response.headers['Access-Control-Allow-Credentials'] = 'true'
+      response.headers['Access-Control-Allow-Headers'] = 'accept, content-type'
+    end
 
     def index
       Jobs.enqueue(:log_dashboard_site, {ip: request.remote_ip, domain: "#{request.protocol}#{request.host_with_port}"})
-      response.headers['Access-Control-Allow-Origin'] = '*'
-      response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
-      response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-CSRF-Token'
-      response.headers['X-Frame-Options'] = "ALLOWALL"
       text = Topic.find(SiteSetting.dashboard_topic_id).first_post.cooked
       render json: {cooked: text}
     end
